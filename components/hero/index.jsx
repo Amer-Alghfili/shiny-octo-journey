c18a3305358f717b5d67a226a866c89b30a034b8
Amer-Alghfili <ameralghfili@gmail.com>
2024-06-07 09:24:38 +0300

    add message in hero component

--- components/hero/index.jsx
+++ components/hero/index.jsx
@@ -1,12 +1,17 @@
 import { IllustrationServiceCard } from "../IllustrationCard";
 
-export default function Hero({ title, src }) {
+export default function Hero({ title, message, src }) {
   return (
     <section>
       <header className="flex flex-col gap-[3.125em] mt-[5.125em]">
-        <h3 className="font-bold text-[2.8125rem] text-center leading-[4.375rem] max-w-[48.866875em]">
-          {title}
-        </h3>
+        <div className="flex flex-col items-center gap-[1.5em]">
+          <h3 className="font-bold text-[2.8125rem] text-center leading-[4.375rem] max-w-[12em]">
+            {title}
+          </h3>
+          {message ? (
+            <div className="max-w-[24.403125em] text-center">{message}</div>
+          ) : null}
+        </div>
         <IllustrationServiceCard src={src} />
       </header>
     </section>
