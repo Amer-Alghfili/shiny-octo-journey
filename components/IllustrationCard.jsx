d628806be4bc5e69613d53a5fb5d00ce2b12a743
Amer-Alghfili <ameralghfili@gmail.com>
2024-06-08 06:23:13 +0300

    some refactor (hero section)

--- components/IllustrationCard.jsx
+++ components/IllustrationCard.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export function IllustrationServiceCard({
-  height = "40em",
+  height = "32em",
   width = "100%",
   src,
 }) {
--- components/hero/index.jsx
+++ components/hero/index.jsx
@@ -2,18 +2,16 @@ import { IllustrationServiceCard } from "../IllustrationCard";
 
 export default function Hero({ title, message, src }) {
   return (
-    <section>
-      <header className="flex flex-col gap-[3.125em] mt-[5.125em]">
-        <div className="flex flex-col items-center gap-[1.5em]">
-          <h3 className="font-bold text-[2.8125rem] text-center leading-[4.375rem] max-w-[12em]">
-            {title}
-          </h3>
-          {message ? (
-            <div className="max-w-[24.403125em] text-center">{message}</div>
-          ) : null}
-        </div>
-        <IllustrationServiceCard src={src} />
+    <section className="flex flex-col gap-[2.90875em] mt-[3em]">
+      <header className="flex flex-col gap-[3.125em] items-center">
+        <h3 className="font-bold text-[2.8125rem] text-center leading-[4.375rem] max-w-[12em]">
+          {title}
+        </h3>
+        {message ? (
+          <div className="max-w-[24.403125em] text-center">{message}</div>
+        ) : null}
       </header>
+      <IllustrationServiceCard src={src} />
     </section>
   );
 }
