ee4b498e15c4856ec2e3516e773c755a68cd9c23
Amer-Alghfili <ameralghfili@gmail.com>
2024-06-08 09:06:42 +0300

    add space between line and heading

--- components/sectionHeader/index.jsx
+++ components/sectionHeader/index.jsx
@@ -1,12 +1,12 @@
 export default function SectionHeader({ title, withMargin = true }) {
   return (
     <div
-      className={`flex flex-col gap-1 items-center`}
+      className={`flex flex-col gap-[1em] items-center`}
       // className={`flex flex-col gap-1 items-center ${
       //   withMargin && "mt-[8.469375em]"
       // }`}
     >
-      <div className="zad h-[2px] w-[15.5em] bg-orange-1" />
+      <div className="zad h-[3px] w-[15.5em] bg-orange-1 rounded-[16px]" />
       <h4 className="text-[2.8125rem] font-bold">{title}</h4>
     </div>
   );
